a75280652d931780d9791f952ed8da178c3dabd9
0400h <git@0400h.cn>
2023-12-12 10:02:36 +0800

    version 0.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "dnndraw",
-    version = "0.1.0",
+    version = "0.2.0",
     keywords = ["pip", "dnndraw", "dnn", "visualize"],
     description = "An editor that visualizes neural networks",
     long_description = "An editor that visualizes neural networks",
